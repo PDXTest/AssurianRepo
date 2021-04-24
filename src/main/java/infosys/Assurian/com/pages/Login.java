4094e29f21d48e0d9ffac774ed1e2464be3bcc68
chandra <chandu@yahoo.com>
2021-04-24 21:24:19 +0530

    go

--- src/main/java/infosys/Assurian/com/pages/Login.java
+++ src/main/java/infosys/Assurian/com/pages/Login.java
@@ -3,38 +3,10 @@ package infosys.Assurian.com.pages;
 public class Login {
 
 	private void syso() {
-		System.out.println("This is Deom");
+		System.out.println("This is Dharma");
 
 	}
 	
 
-	private void syso1() {
-		System.out.println("This is Deom");
-		System.out.println("This is Deom1");
-	}
-	
-	private void syso2() {
-		System.out.println("This is Deom");
-		System.out.println("This is Deom1");
-	}
-	
 
-	private void syso12() {
-		System.out.println("This is Deom");
-		System.out.println("This is Deom1");
-	}
-	
-	private void syso25656() {
-		System.out.println("This is Deom");
-		System.out.println("This is Deom1");
-	}
-	
-	private void DONMinakshi() {
-		System.out.println("This is Deom");
-		System.out.println("This is Deom1");
-	}
-	private void DONMinakshinrrrrrrr() {
-		System.out.println("This is Deom");
-		System.out.println("This is Deom1");
-	}
 }
