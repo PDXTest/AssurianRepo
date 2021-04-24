073f8ffa3c5000774eb14024c23a07f7487b04c1
chandra <chandu@yahoo.com>
2021-04-24 19:33:32 +0530

    hi

---
+++ src/main/java/infosys/Assurian/com/pages/Login.java
@@ -0,0 +1,22 @@
+package infosys.Assurian.com.pages;
+
+public class Login {
+
+	private void syso() {
+		System.out.println("This is Deom");
+
+	}
+	
+
+	private void syso1() {
+		System.out.println("This is Deom");
+		System.out.println("This is Deom1");
+	}
+	
+	private void syso2() {
+		System.out.println("This is Deom");
+		System.out.println("This is Deom1");
+	}
+	
+	
+}
