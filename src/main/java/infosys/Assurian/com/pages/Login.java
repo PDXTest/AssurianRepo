4316603ac4396e14162d05be0462d386f8aeca34
MinaKSHI bHIRUD <chandu.bhirud.pune@gmail.com>
2021-06-06 16:34:28 +0530

    hi

--- src/main/java/infosys/Assurian/com/pages/Login.java
+++ src/main/java/infosys/Assurian/com/pages/Login.java
@@ -10,15 +10,13 @@ public class Login {
 	
 	private void syso1() {
 		System.out.println("This is Dharma");
-				System.out.println("This is Dharma");
+		System.out.println("This is Dharma");
 
 
 	}
 	
 	private void syso13456789() {
 		System.out.println("This is Dharma");
-				System.out.println("This is Dharma");
-
 
 	}
 
